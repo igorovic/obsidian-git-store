a701d62d4a09871a806e309c013047654f0fd61e
igorovic <758832+igorovic@users.noreply.github.com>
2021-04-05 15:49:44 +0200

    initial

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { App, ButtonComponent, Modal, Notice, Plugin, PluginSettingTab, Setting, TextComponent } from 'obsidian';
+
+
+export default class GitStore extends Plugin {
+    async onload() {
+		console.log('Loading git storex§.');
+    }
+}
