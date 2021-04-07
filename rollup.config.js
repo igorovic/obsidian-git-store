d2d36c36303256a18d29072095932fa60506dda4
igorovic <758832+igorovic@users.noreply.github.com>
2021-04-07 19:36:24 +0200

    wip

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,55 @@
-import typescript from '@rollup/plugin-typescript';
-import {nodeResolve} from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import scss from 'rollup-plugin-scss';
+import typescript from "@rollup/plugin-typescript";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import { terser } from "rollup-plugin-terser";
+import scss from "rollup-plugin-scss";
+import { writeFileSync } from "fs";
+import path from "path";
 
-export default {
-  input: 'src/main.ts',
-  output: {
-    file: "main.js",
-    sourcemap: 'inline',
-    format: 'cjs',
-    exports: 'default'
-  },
-  external: ['obsidian'],
+const TEST_VAULT_PLUGIN = "../test_vault/.obsidian/plugins/obsidian-git-store/";
+const isDev = String(process.env.NODE_ENV).toLowerCase() === "development";
+const plugins = [];
+
+if (!isDev) {
+  plugins.push(terser());
+}
+
+const Config = {
+  input: "src/main.ts",
+  output: [
+    {
+      file: "dist/main.js",
+      sourcemap: "inline",
+      format: "cjs",
+      exports: "default",
+      plugins,
+    },
+  ],
+  external: ["obsidian", "fs", "path", "util"],
   plugins: [
     typescript(),
-    nodeResolve({browser: true}),
+    nodeResolve({ browser: true }),
     commonjs(),
-    scss({ output: 'styles.css', sass: require('sass') })
-  ]
+    scss({
+      output: function (styles) {
+        writeFileSync("dist/styles.css", styles);
+        if (isDev) {
+          writeFileSync(path.join(TEST_VAULT_PLUGIN, "styles.css"), styles);
+        }
+      },
+      sass: require("sass"),
+    }),
+  ],
 };
+
+if (isDev) {
+  Config.output.push({
+    file: path.join(TEST_VAULT_PLUGIN, "main.js"),
+    sourcemap: "inline",
+    format: "cjs",
+    exports: "default",
+    plugins,
+  });
+}
+
+export default Config;
